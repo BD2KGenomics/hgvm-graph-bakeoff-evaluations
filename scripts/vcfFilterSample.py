7e02a42f91e42ce27deea8a8bf8849ded5e33747
Glenn Hickey <hickey@soe.ucsc.edu>
2015-11-18 09:07:06 -0800

    add script and data for producing 1000 genomes sample vcfs

---
+++ scripts/vcfFilterSample.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2.7
+
+"""
+ Filter a sample out of a vcf, keeping only snps that are present in 
+ genotype information for that sample.  
+
+ https://github.com/ekg/vcflib must be installed (with vcflib/bin) in PATH. 
+"""
+
+
+import argparse, sys, os, os.path, random, subprocess, shutil, itertools
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description=__doc__, 
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument("vcf", type=str,
+                        help="Input vcf file"),
+    parser.add_argument("sample", type=str,
+                        help="Sample ID to extract")
+    parser.add_argument("--dot", action="store_true", default=False,
+                        help="Accept GT ., GT ./. and GT 0|. genotypes")    
+    args = args[1:]
+    options = parser.parse_args(args)
+    return options
+
+def main(args):
+    options = parse_args(args)
+
+    filterCmd = "vcfkeepsamples {} {}".format(options.vcf, options.sample)
+
+    filterProc = subprocess.Popen(filterCmd, shell=True, stdout=subprocess.PIPE)
+
+    while True:
+        line = filterProc.stdout.readline()
+        if line != "":
+	    if line.find("GT\t0|0\n") < 0 and (
+                # not too sure what to make of these genotypes,
+                # so toggle with "dot" option for now. 
+	        options.dot or (line.find("GT\t.\n") < 0 and
+				line.find("GT\t.|.\n") < 0 and
+                                line.find("GT\t.|0\n") < 0 and
+                                line.find("GT\t0|.\n") < 0)):
+                sys.stdout.write(line)
+        else:
+            break
+	 
+if __name__ == "__main__" :
+    sys.exit(main(sys.argv))
