45f5c9f9d9644a0f5728e30defccc6b61d604be4
Glenn Hickey <hickey@soe.ucsc.edu>
2015-12-09 09:21:26 -0800

    update compare script

--- scripts/compare_snps.sh
+++ scripts/compare_snps.sh
@@ -10,7 +10,7 @@ ALIGNMENTS=$2
 VARIANTS=$3
 OUT_DIR=$4
 TOIL_DIR=cps_toil_dir
-TOIL_OPTS="--maxCores 48 --vg_cores 8"
+TOIL_OPTS="--maxCores 48 --vg_cores 4"
 INDEX_OPTS="--kmer 27 --edge_max 3"
 TAG_OPTS="--orig_tag ${GRAPHS}"
 #OPTS="--dir_tag --only_summary"
@@ -20,20 +20,24 @@ REGIONS=( "brca1" "brca2" "sma" "lrc_kir" "mhc" )
 #REGIONS=( "brca1" )
 
 # output for clusteGraphs.py that we rename
-CP_FILES=( "heatmap_kmer.pdf" "heatmap_log_kmer.pdf" "heatmap_vm1_kmer.pdf" "heatmap_log_vm1_kmer.pdf" "tree_kmer.dot" "tree_kmer.newick"  "tree_kmer.png" "heatmap_corg.pdf" "heatmap_log_corg.pdf" "heatmap_vm1_corg.pdf" "heatmap_log_vm1_corg.pdf" "tree_corg.dot" "tree_corg.newick"  "tree_corg.png" )
+CP_FILES=( "heatmap_kmer.pdf" "heatmap_log_kmer.pdf" "heatmap_vm1_kmer.pdf" "heatmap_log_vm1_kmer.pdf" "tree_kmer.dot" "tree_kmer.newick"  "tree_kmer.png" "heatmap_corg.pdf" "heatmap_log_corg.pdf" "heatmap_vm1_corg.pdf" "heatmap_log_vm1_corg.pdf" "tree_corg.dot" "tree_corg.newick"  "tree_corg.png" "heatmap_corg1.pdf" "heatmap_log_corg1.pdf" "heatmap_vm1_corg1.pdf" "heatmap_log_vm1_corg1.pdf" "tree_corg1.dot" "tree_corg1.newick"  "tree_corg1.png")
+
+# vglr lrc_kir is a bad graph.  we can censor it as input in the wildcard arguments below
+# to make it disappear from the analysis
+GLOBIGNORE="*/lrc_kir/vglr*:*vglr-lrc_kir*"
 
 # These are the g1kvcf samples that don't exist.  right now heatmaps are just computed without
 # them.  so squares compareing g1kvcf samples will be average of 5 datapoints whereas all
 # other squares will ne average of 9 datapoints.  We can ensure only 5 datapoints ever used
 # by uncommenting this, but it seems a bit drastic
-#GLOBIGNORE="*HG00512*:*HG00514*:*HG00733*:*NA19240*"
+#GLOBIGNORE="*HG00512*:*HG00514*:*HG00733*:*NA19240*":${GLOBIGNORE}
 
 mkdir $OUT_DIR
 
 for i in "${REGIONS[@]}"
 do
 	 #heatmap of original
-	 rm -rf ${TOIL_DIR} ; scripts/clusterGraphs.py ./${TOIL_DIR} ${GRAPHS}/*${i}*.vg ${OUT_DIR}/${i} ${TAG_OPTS} ${TOIL_OPTS} ${INDEX_OPTS} ${OPTS}
+	 rm -rf ${TOIL_DIR} ; scripts/clusterGraphs.py ./${TOIL_DIR} ${GRAPHS}/*${i}*.vg ${OUT_DIR}/${i} ${TOIL_OPTS} ${INDEX_OPTS} --no_corg
 
 	 for j in "${CP_FILES[@]}"
 	 do
@@ -45,15 +49,15 @@ do
 	 for k in sample
 	 do
 		  # just the category
-		  rm -rf ${TOIL_DIR} ; scripts/clusterGraphs.py ./${TOIL_DIR} ${VARIANTS}/${i}/*/*${k}*.vg ${OUT_DIR}/${i} ${TAG_OPTS} ${TOIL_OPTS} ${INDEX_OPTS} ${OPTS} --avg_sample --no_corg
+		  rm -rf ${TOIL_DIR} ; scripts/clusterGraphs.py ./${TOIL_DIR} ${VARIANTS}/${i}/*/*${k}*.vg ${OUT_DIR}/${i} ${TAG_OPTS} ${TOIL_OPTS} ${INDEX_OPTS} ${OPTS} --avg_sample --no_corg --skip g1kvcf
 
 		  for j in "${CP_FILES[@]}"
 		  do
 				mv ${OUT_DIR}/${i}/${j} ${OUT_DIR}/${i}/${k}_${i}_avg_${j}
 		  done
 
-		  # category and original together
-		  rm -rf ${TOIL_DIR} ; scripts/clusterGraphs.py ./${TOIL_DIR} ${GRAPHS}/*${i}*.vg ${VARIANTS}/${i}/*/*${k}*.vg ${OUT_DIR}/${i} ${TAG_OPTS} ${TOIL_OPTS} ${INDEX_OPTS} ${OPTS} --avg_sample --no_corg
+		  #category and original together
+		  rm -rf ${TOIL_DIR} ; scripts/clusterGraphs.py ./${TOIL_DIR} ${GRAPHS}/*${i}*.vg ${VARIANTS}/${i}/*/*${k}*.vg ${OUT_DIR}/${i} ${TAG_OPTS} ${TOIL_OPTS} ${INDEX_OPTS} ${OPTS} --avg_sample --no_corg --skip g1kvcf
 
 		  for j in "${CP_FILES[@]}"
 		  do
@@ -63,7 +67,7 @@ do
 
 	 # tables
 	 mkdir ${OUT_DIR}/${i}
-	 scripts/callStats.py ${ALIGNMENTS}/${i}/*/*.gam --out_dir ${VARIANTS}  --out_sub $i  --graph_dir ${GRAPHS}
+	 scripts/callStats.py ${ALIGNMENTS}/${i}/*/*.gam --out_dir ${VARIANTS}  --out_sub $i  --graph_dir ${GRAPHS} --avg_sample
 	 rm -rf ${TOIL_DIR} ; scripts/trioStats.py ./${TOIL_DIR} ${ALIGNMENTS}/${i}/*/*.gam --out_dir ${VARIANTS} --out_sub $i ${TOIL_OPTS}
 	 cp ${VARIANTS}/${i}/*.tsv ${VARIANTS}/${i}/*.pdf ${OUT_DIR}/${i}
 	 	 
